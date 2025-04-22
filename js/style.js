fb042eb15b45e26dcdea703b613570966ef23d66
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-04-22 22:14:56 +0900

    タイマーの基礎の動き

--- js/style.js
+++ js/style.js
@@ -1,4 +1,11 @@
 
+// .view
+const viewArea = document.querySelector(".view");
+//.btn_start
+const startButton = document.querySelector(".btn_start");
+//.btn_stop
+const stopButton =  document.querySelector(".btn_stop");
+
 function createLines () {
     let line = document.createElement("div");
     line.setAttribute("class", "line");
@@ -20,3 +27,59 @@ function createLines () {
 setInterval(function() {
     createLines();
 }, 200);
+
+
+let timer;
+let isRunning = false;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+// htmlから要素の取得
+const timerElement = document.addEventListener("timer") ;
+const start = document.addEventListener("start");
+const reset = document.addEventListener("reset");
+
+start.addEventListener("click",()=>{
+    if(isRunning){
+        // タイマーが起動していない状態
+        clearInterval(timer);
+        start.textContent = "start";
+    }else{
+        // タイマーが起動している時
+        timer = setInterval(updateTimer,10);
+        start.textContent = "stop"
+    }
+});
+
+//resetボタンが押された時の関数設定
+reset.addEventListener('click',resetTimer);
+
+//タイマーの数値の表示
+function updateTimer(){
+    seconds++;
+    if(seconds === 99){
+        seconds = 0;
+        minutes++;
+        if(minutes === 99){
+            minutes = 0;
+            hours++;
+        }
+    }
+    timerElement.textContent = formatTime(hours) +':'+ formatTime(minutes) +':'+ formatTime(seconds);
+}
+
+function formatTime(time){
+    return time < 10 ? '0' + time : time;
+}
+
+//resetボタンの機能
+function resetTimer(){
+    clearInterval(timer);
+    isRunning = false;
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    timerElement.textContent = '00:00:00'
+    start.textContent = 'start';
+}
